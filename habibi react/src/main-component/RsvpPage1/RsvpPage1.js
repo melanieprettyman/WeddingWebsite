28b139ab783405e2380c0382f6fc4a9b157cf4fc
mprettyman <mprettyman@nobellabs.com>
2025-02-05 11:36:48 -0700

    added child-free comment to RSVP

--- habibi react/src/main-component/RsvpPage1/RsvpPage1.js
+++ habibi react/src/main-component/RsvpPage1/RsvpPage1.js
@@ -1,20 +1,25 @@
-import React, { Fragment } from 'react';
-import PageTitle from '../../components/pagetitle/PageTitle';
-import Navbar from '../../components/Navbar/Navbar';
-import Footer from '../../components/footer/Footer'
-import Scrollbar from '../../components/scrollbar/scrollbar'
-import RsvpSection from '../../components/RsvpSection/RsvpSection';
+import React, { Fragment } from "react";
+import PageTitle from "../../components/pagetitle/PageTitle";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/footer/Footer";
+import Scrollbar from "../../components/scrollbar/scrollbar";
+import RsvpSection from "../../components/RsvpSection/RsvpSection";
+import { RSVPConstants } from "../../constants";
 
 const RsvpPage1 = () => {
-    return (
-        <Fragment>
-            <Navbar hclass={'wpo-header-style-3'}/>
-            <PageTitle pageTitle={'R.S.V.P'} pagesub={'RSVP'} />
-            <RsvpSection rClass={'pt-120'}/>
-            <Footer/>
-            <Scrollbar />
-        </Fragment>
-    )
+  return (
+    <Fragment>
+      <Navbar hclass={"wpo-header-style-3"} />
+      <PageTitle
+        pageTitle={"R.S.V.P"}
+        pagesub={"RSVP"}
+        content={RSVPConstants.subTitle}
+      />
+      <RsvpSection rClass={"pt-120"} />
+      <Footer />
+      <Scrollbar />
+    </Fragment>
+  );
 };
 
 export default RsvpPage1;
