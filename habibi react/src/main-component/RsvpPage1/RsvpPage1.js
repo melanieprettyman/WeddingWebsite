037b56bf0d05477b7ea9f9fa60920dedccdfb580
melanieprettyman <melanie.prettyman98@gmail.com>
2025-02-09 15:44:49 -0700

    child care rsvp msg

--- habibi react/src/main-component/RsvpPage1/RsvpPage1.js
+++ habibi react/src/main-component/RsvpPage1/RsvpPage1.js
@@ -1,20 +1,26 @@
-import React, { Fragment } from 'react';
-import PageTitle from '../../components/pagetitle/PageTitle';
-import Navbar from '../../components/Navbar/Navbar';
-import Footer from '../../components/footer/Footer'
-import Scrollbar from '../../components/scrollbar/scrollbar'
-import RsvpSection from '../../components/RsvpSection/RsvpSection';
+import React, { Fragment } from "react";
+import PageTitle from "../../components/pagetitle/PageTitle";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/footer/Footer";
+import Scrollbar from "../../components/scrollbar/scrollbar";
+import RsvpSection from "../../components/RsvpSection/RsvpSection";
 
 const RsvpPage1 = () => {
-    return (
-        <Fragment>
-            <Navbar hclass={'wpo-header-style-3'}/>
-            <PageTitle pageTitle={'R.S.V.P'} pagesub={'RSVP'} />
-            <RsvpSection rClass={'pt-120'}/>
-            <Footer/>
-            <Scrollbar />
-        </Fragment>
-    )
+  return (
+    <Fragment>
+      <Navbar hclass={"wpo-header-style-3"} />
+      <PageTitle
+        pageTitle={"R.S.V.P"}
+        content={
+          "Please note that this is an adults-only event, and we kindly ask that you make arrangements for childcare."
+        }
+        pagesub={"RSVP"}
+      />
+      <RsvpSection rClass={"pt-120"} />
+      <Footer />
+      <Scrollbar />
+    </Fragment>
+  );
 };
 
 export default RsvpPage1;
